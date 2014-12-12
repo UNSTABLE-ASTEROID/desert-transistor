240d2876581abd610041853ad79683781ead259f
Christopher Treviño <christophertrev@gmail.com>
2014-12-11 18:30:26 -0800

    (feat) show data from server in teacher view

--- client/App/Teacher/teacherCtrl.js
+++ client/App/Teacher/teacherCtrl.js
@@ -1,8 +1,13 @@
 angular
 	  .module('TeacherController', [])
-	  .controller('TeacherController', ['$scope', 'teacherFactory', function($scope, teacherFactory){
+	  .controller('TeacherController', ['$scope', 'teacherFactory','$http', function($scope, teacherFactory, $http){
 
-      $scope.lectures = ['bio','phys','chem']
+      // $scope.lectures = ['bio','phys','chem']
 			$scope.confusedStudents = teacherFactory.confusedStudents;
+      $http.get('/api/teachers')
+      .success(function(data, status, headers, config) {
+        console.log('http',data)
+        $scope.lectures = data;
+      })
 			
 	  }])
