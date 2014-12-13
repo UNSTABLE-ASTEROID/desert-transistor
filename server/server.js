65e192524cd52ca9ed858c826e2c05ad91e26dc2
Christopher Treviño <christophertrev@gmail.com>
2014-12-12 17:01:30 -0800

    (docs) created docs

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 var voteRouter = express.Router();
 app.use('/api', voteRouter);
+app.use('/docs', express.static(__dirname + '/../docs'));
 
 ///////////////////////////    DATABASE    ///////////////////////////
 //uses the url from the config file to connect to Mongo Labs
